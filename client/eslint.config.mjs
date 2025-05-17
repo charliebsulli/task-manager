d8fa92948bbd4d70354561e09adef7c7a722d388
Charles <charlessullivan@brandeis.edu>
2025-05-16 22:23:51 -0700

    Bootstrapped client with create-next-app

---
+++ client/eslint.config.mjs
@@ -0,0 +1,20 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+import reactHooks from 'eslint-plugin-react-hooks';
+import eslintConfigPrettier from "eslint-config-prettier";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  reactHooks.configs['recommended-latest'],
+  eslintConfigPrettier,
+];
+
+export default eslintConfig;
