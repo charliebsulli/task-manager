f6abaee7b4cd2deb59e52011f25d8270aef524db
Charles <charlessullivan@brandeis.edu>
2025-05-28 12:55:03 -0700

    Logged-in user can access + modify their tasks and tags

--- client/src/api/api-client.ts
+++ client/src/api/api-client.ts
@@ -9,5 +9,18 @@ export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
-// all of my requests need auth header
-// if there is no auth header, must redirect to login page
+api.interceptors.request.use((config) => {
+  const accessToken = localStorage.getItem("accessToken");
+  if (accessToken) {
+    config.headers.Authorization = accessToken;
+  }
+  return config;
+});
+
+api.interceptors.response.use();
+// if a user is logged in, send an authentication header
+// with the JWT with every request
+
+// if the JWT is expired, send a refresh token to /refresh-token
+
+// if the JWT is invalid, clear it, redirect to login page
