e8eabe64d5c9b45d6e89663cf88e054bf6a7435a
Charles <charlessullivan@brandeis.edu>
2025-06-11 14:23:45 -0700

    Debugging ENV vars

--- client/src/api/api-client.ts
+++ client/src/api/api-client.ts
@@ -5,7 +5,7 @@ export interface Credentials {
   password: string;
 }
 
-console.log(process.env);
+console.log(process.env.NEXT_PUBLIC_API_URL);
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
